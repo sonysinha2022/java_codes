e77d1d17a85d056a3e8dc7efcbdf8edf5451c61a
Sony Sinha <107948215+sonysinha2022@users.noreply.github.com>
2023-10-23 00:05:30 +0530

    Add files via upload

---
+++ TestPoint.java
@@ -0,0 +1,19 @@
+package assign;
+
+public class TestPoint {
+
+	public static void main(String[] args) {
+		Point po =new Point(4,6);
+		po.Display();
+		Point p = new Point();
+		p.getX();
+		p.getY();
+		p.setX(7);
+		p.setY(10);
+		p.Display();
+
+	}
+
+}
+
+
