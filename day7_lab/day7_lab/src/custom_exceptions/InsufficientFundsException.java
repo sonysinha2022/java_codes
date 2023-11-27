375e6ba5ce5f12a6b5d536db2383f9c9355f6782
monali <107948215+GargiKumari2024@users.noreply.github.com>
2023-11-28 00:16:17 +0530

    Delete day7_lab directory

--- day7_lab/day7_lab/src/custom_exceptions/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package custom_exceptions;
-
-public class InsufficientFundsException extends Exception {
-	public InsufficientFundsException(String mesg) {
-		super(mesg);
-	}
-}
--- day7_lab/day7_lab/src/custom_exceptions/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package custom_exceptions;
-
-public class InvalidInputException extends Exception {
-	public InvalidInputException(String mesg) {
-		super(mesg);
-	}
-}
