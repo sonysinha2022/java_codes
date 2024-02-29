bd338d4f7796fb4c15f09f28b8ccc02833d46af4
Sony Sinha <sonysinha932@gmail.com>
2024-03-01 00:19:36 +0530

    LongestPalindromic

---
+++ LongestPalindrome.java
@@ -0,0 +1,43 @@
+package ert;
+
+
+
+
+import java.util.Scanner;
+
+public class LongestPalindrome  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String longestPalindrome = findLongestPalindrome(input);
+        System.out.println("Longest palindromic substring: " + longestPalindrome);
+    }
+
+    private static String findLongestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+               start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
